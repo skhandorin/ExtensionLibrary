ca3cfb1ce16f20a4cf808fe6fe8a10bf25ddc1b5
skhandorin <hsatig@mail.ru>
2017-03-21 17:58:44 +0500

    Modle 4 - Demo 2  Reflection and Expressions - beginning

---
+++ ExtensionLibrary/ExtensionLibrary.Tests/Demo2/Map.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionLibrary.Tests.Demo2
+{
+    public class Map<TSource, TTarget> 
+        where TTarget : class, new()
+    {
+        public TSource Source { get; set; }
+        public TTarget Target { get; set; }
+
+        public Map(TSource source)
+        {
+            Source = source;
+            Target = new TTarget();
+        }
+
+        public Map<TSource, TTarget> Populate(string targetPropertyName, string sourcePropertyName)
+        {
+            var targetAccessor = typeof(TTarget).GetProperty(targetPropertyName);
+            var sourceAccessor = typeof(TSource).GetProperty(sourcePropertyName);
+            targetAccessor.SetValue(Target, sourceAccessor.GetValue(Source));
+            return this;
+        }
+
+        public Map<TSource, TTarget> Populate<T>(string targetPropertyName, Func<TSource, T> sourceValue)
+        {
+            var targetAccessor = typeof(TTarget).GetProperty(targetPropertyName);
+            targetAccessor.SetValue(Target, sourceValue(Source));
+            return this;
+        }
+    }
+}
