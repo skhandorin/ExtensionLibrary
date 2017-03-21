f9c136120e3802345aac557df63c7c79c7749184
skhandorin <hsatig@mail.ru>
2017-03-21 17:33:18 +0500

    Enum Extensions

--- ExtensionLibrary/ExtensionLibrary.Tests/Demo1/EnumExtensionsTests.cs
+++ ExtensionLibrary/ExtensionLibrary.Tests/Demo1/EnumExtensionsTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using componentModel = System.ComponentModel;
 
 namespace ExtensionLibrary.Tests.Demo1
 {
@@ -7,8 +8,27 @@ namespace ExtensionLibrary.Tests.Demo1
     public class EnumExtensionsTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void GetName()
         {
+            var intro = Module.Intro;
+            Assert.AreEqual("Intro", intro.ToString());
+            Assert.AreEqual("Intro", Enum.GetName(typeof(Module), intro));
+            Assert.AreEqual("Intro", intro.GetName());
+        }
+
+        [TestMethod]
+        public void GetDescription()
+        {
+            Assert.AreEqual("Introducing Extension Methods", Module.Intro.GetDescription());
+            Assert.AreEqual("Advanced", Module.Advanced.GetDescription());
+        }
+
+        public enum Module
+        {
+            [componentModel.Description("Introducing Extension Methods")]
+            Intro,
+            Advanced,
+            Library
         }
     }
 }
--- ExtensionLibrary/ExtensionLibrary/Demo1/EnumExtensions.cs
+++ ExtensionLibrary/ExtensionLibrary/Demo1/EnumExtensions.cs
@@ -1,12 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ExtensionLibrary.Demo1
+namespace System
 {
-    class EnumExtensions
+    public static class EnumExtensions
     {
+        public static string GetName(this Enum value)
+        {
+            return Enum.GetName(value.GetType(), value);
+        }
+
+        public static string GetDescription(this Enum value)
+        {
+            var fieldInfo = value.GetType().GetField(value.GetName());
+            var descriptionAttribute = fieldInfo.GetCustomAttributes(typeof(DescriptionAttribute), false).FirstOrDefault() as DescriptionAttribute;
+            return descriptionAttribute == null ? value.GetName() : descriptionAttribute.Description;
+        }
     }
 }
