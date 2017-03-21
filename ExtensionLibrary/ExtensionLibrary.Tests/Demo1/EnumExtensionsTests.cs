fda591bbd4cf4ef7a8da2ccac7beafd7be2d9f45
skhandorin <hsatig@mail.ru>
2017-03-21 17:03:02 +0500

    Module 4 - Extension Library - ExceptionExtensions

---
+++ ExtensionLibrary/ExtensionLibrary.Tests/Demo1/EnumExtensionsTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ExtensionLibrary.Tests.Demo1
+{
+    [TestClass]
+    public class EnumExtensionsTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ ExtensionLibrary/ExtensionLibrary/Demo1/ExceptionExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System
+{
+    public static class ExceptionExtensions
+    {
+        public static string FullMessage(this Exception ex)
+        {
+            var builder = new StringBuilder();
+            while (ex != null)
+            {
+                builder.AppendFormat("{0}{1}", ex.Message, Environment.NewLine);
+                ex = ex.InnerException;
+            }
+            return builder.ToString();
+        }
+    }
+}
